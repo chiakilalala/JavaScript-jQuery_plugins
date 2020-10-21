9c3e62451093bcc96092eafd278f1f979cb0bb24
yilin <chiaki205102@gmail.com>
2020-10-21 16:58:29 +0800

    change js

--- 002InfiniteCarousel/js/all.js
+++ 002InfiniteCarousel/js/all.js
@@ -1,55 +1,44 @@
-$(document).ready(function() {
-    var carousel = $('#carousel'),
-        threshold = 150,
-        slideWidth = 500,
-        dragStart,
-        dragEnd;
-    //step1  push verb
-    $('#next').click(function() {
-        shiftSlide(-1);
-    });
-    $('#prev').click(function() {
-        shiftSlide(1);
-    });
-    carousel.on('mousedown', function(e) { //按下
-            if (carousel.hasClass('transition')) return;
-            dragStart = e.pageX;
-            $(this).on('mousemove', function(e) {
-                // console.log(this);
-                dragEnd = e.pageX;
-                $(this).css('transform', 'translateX(' + dragPos() + 'px)')
-                console.log(dragStart, 'dragStart');
-                console.log(dragEnd, 'dragEnd');
-            });
+var carousel = $('#carousel'),
+    threshold = 150,
+    slideWidth = 500,
+    dragStart,
+    dragEnd;
 
-            $(document).on('mouseup', function() {
-                if (dragPos() > threshold) { return shiftSlide(1) }
-                if (dragPos() < -threshold) { return shiftSlide(-1) }
-                shiftSlide(0);
-            });
+$('#next').click(function() { shiftSlide(-1) })
+$('#prev').click(function() { shiftSlide(1) })
 
-        })
-        //dragposition
-    function dragPos() {
-        return dragEnd - dragStart;
-    }
-    // shiftSlide 
-    function shiftSlide(direction) {
-        if (carousel.hasClass('transition')) return;
-        dragEnd = dragStart;
-        $(document).off('mouseup')
-        carousel.off('mousemove')
-            .addClass('transition')
-            .css('transform', 'translateX(' + (direction * slideWidth) + 'px)');
-        setTimeout(() => {
-            if (direction === 1) {
-                $('.slide:first').before($('.slide:last'));
-            } else if (direction === -1) {
-                $('.slide:last').after($('.slide:last'));
-            }
-            carousel.removeClass('transition')
-            carousel.css('transform', 'translateX(0px)');
-        }, 700);
-
-    }
+carousel.on('mousedown', function() {
+    if (carousel.hasClass('transition')) return;
+    dragStart = event.pageX;
+    $(this).on('mousemove', function() {
+        dragEnd = event.pageX;
+        $(this).css('transform', 'translateX(' + dragPos() + 'px)')
+    })
+    $(document).on('mouseup', function() {
+        if (dragPos() > threshold) { return shiftSlide(1) }
+        if (dragPos() < -threshold) { return shiftSlide(-1) }
+        shiftSlide(0);
+    })
 });
+
+function dragPos() {
+    return dragEnd - dragStart;
+}
+
+function shiftSlide(direction) {
+    if (carousel.hasClass('transition')) return;
+    dragEnd = dragStart;
+    $(document).off('mouseup')
+    carousel.off('mousemove')
+        .addClass('transition')
+        .css('transform', 'translateX(' + (direction * slideWidth) + 'px)');
+    setTimeout(function() {
+        if (direction === 1) {
+            $('.slide:first').before($('.slide:last'));
+        } else if (direction === -1) {
+            $('.slide:last').after($('.slide:first'));
+        }
+        carousel.removeClass('transition')
+        carousel.css('transform', 'translateX(0px)');
+    }, 700)
+}
