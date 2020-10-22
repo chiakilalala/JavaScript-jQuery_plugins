595f547dfeec4312fd6b7aab505dba6a1a3df0fa
yilin <chiaki205102@gmail.com>
2020-10-22 18:11:21 +0800

    增加香草輪播圖

--- 002InfiniteCarousel/js/vanilla.js
+++ 002InfiniteCarousel/js/vanilla.js
@@ -1,32 +1,71 @@
-/* DOMContentLoaded 事件是在 DOM 結構被完整的讀取跟解析後就會被觸發，
-不須等待外部資源讀取完成*/
-
 var index = 0,
     amount = 0,
+    currTransl = [],
     translationComplete = true,
     moveOffset = 0;
 
-document.addEventListener('DOMContentLoaded', function(e) {
+var transitionCompleted = function() {
+        translationComplete = true;
+    }
+    /*DOMContentLoaded全部建立之後在仔入*/
+document.addEventListener("DOMContentLoaded", function(event) {
     var carousel = document.getElementById('carousel');
-    amount = document.getElementsByClassName('slide').length; //總長度
+
+    amount = document.getElementsByClassName("slide").length; /*全部幻燈片數目 */
+    // 取得全部容器寬度
     moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
+    // 計算幻燈片寬度
     document.getElementById('carousel').style.width = (amount * moveOffset) + 'px';
+    // 避免點擊時干擾轉換
     for (var i = 0; i < amount; i++) {
         currTransl[i] = -moveOffset;
         document.getElementsByClassName("slide")[i].addEventListener("transitionend", transitionCompleted, true);
         document.getElementsByClassName("slide")[i].addEventListener("webkitTransitionEnd", transitionCompleted, true);
         document.getElementsByClassName("slide")[i].addEventListener("oTransitionEnd", transitionCompleted, true);
         document.getElementsByClassName("slide")[i].addEventListener("MSTransitionEnd", transitionCompleted, true);
-
     }
     // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
     document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[4], document.getElementById('carousel').children[0])
-        // add click events to control arrows
+        //增加按鈕點擊
     document.getElementById('prev').addEventListener('click', prev, true);
     document.getElementById('next').addEventListener('click', next, true);
-
 });
 
 function prev() {
+    if (translationComplete === true) {
+        translationComplete = false;
+        index--;
+        if (index == -1) {
+            index = amount - 1;
+        }
+        var outerIndex = (index) % amount;
+        for (var i = 0; i < amount; i++) {
+            var slide = document.getElementsByClassName("slide")[i];
+            slide.style.opacity = '1';
+            slide.style.transform = 'translateX(' + (currTransl[i] + moveOffset) + 'px)';
+            currTransl[i] = currTransl[i] + moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX(' + (currTransl[outerIndex] - (moveOffset * amount)) + 'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex] - moveOffset * (amount);
+    }
+}
 
+function next() {
+    if (translationComplete === true) {
+        translationComplete = false;
+        var outerIndex = (index) % amount;
+        index++;
+        for (var i = 0; i < amount; i++) {
+            var slide = document.getElementsByClassName("slide")[i];
+            slide.style.opacity = '1';
+            slide.style.transform = 'translateX(' + (currTransl[i] - moveOffset) + 'px)';
+            currTransl[i] = currTransl[i] - moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX(' + (currTransl[outerIndex] + (moveOffset * amount)) + 'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex] + moveOffset * (amount);
+    }
 }
